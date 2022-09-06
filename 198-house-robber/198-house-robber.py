b602899df058d259a0b483d19ac860c810a8df7f
Kushagras634 <60312895+Kushagras634@users.noreply.github.com>
2022-09-06 12:07:12 +0530

    Time: 58 ms (27.70%), Space: 13.8 MB (97.19%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,17 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        inclusive=nums[0]
+        exclusive=0
         n=len(nums)
-        if n==1:
-            return nums[0]
-        if n==2:
-            return max(nums[0],nums[1])
-        else:
-            for i in range(2,n):
-                if i==2:
-                    x=nums[i-2]
-                    nums[i]+=x
-                else:
-                    x=max(nums[i-2],nums[i-3])
-                    nums[i]+=x
-        return max(nums[n-1],nums[n-2])
+        for i in range(1,n):
+            x=inclusive
+            inclusive=exclusive+nums[i]
+            exclusive=max(x,exclusive)
+        return max(inclusive,exclusive)
         
