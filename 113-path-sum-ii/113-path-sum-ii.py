f4f38019e3fe740486985dca2301e13d24d842fd
Kushagras634 <60312895+Kushagras634@users.noreply.github.com>
2022-09-24 23:43:06 +0530

    Time: 90 ms (26.35%), Space: 15.7 MB (52.98%) - LeetHub

---
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def pathSum(self, R: TreeNode, S: int) -> List[List[int]]:
+        A, P = [], []
+        def dfs(N):
+            if N == None: return
+            P.append(N.val)
+            if (N.left,N.right) == (None,None) and sum(P) == S: A.append(list(P))
+            else: dfs(N.left), dfs(N.right)
+            P.pop()
+        dfs(R)
+        return A
